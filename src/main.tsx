6d9f41ea185e3932caa0f2d699ccf7875b50c3d2
bbearcookie <aodem@naver.com>
2024-01-16 03:44:39 +0900

    📦 Chore: TanStack Query V5 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import router from './router';
 import 'normalize.css';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
