c961147c65989d865a81e1a71a670a4088c7d8b2
bbearcookie <aodem@naver.com>
2024-01-16 01:53:51 +0900

    📦 Chore: emotion 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import EmotionTestButton from '@/components/EmotionTestButton';
+
 function App() {
   return (
     <>
       <h1>Hello World</h1>
+      <EmotionTestButton>이모션 테스트</EmotionTestButton>
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
