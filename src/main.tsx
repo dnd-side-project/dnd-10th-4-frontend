5246bd0b70a8491fc8c41a027be8a3824356a222
bbearcookie <aodem@naver.com>
2024-01-16 04:08:16 +0900

    📦 Chore: recoil 설치

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RecoilRoot } from 'recoil';
 import router from './router';
 import 'normalize.css';
 
@@ -11,8 +12,10 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools />
+      <RecoilRoot>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </RecoilRoot>
     </QueryClientProvider>
   </React.StrictMode>,
 );
