b00ae81ea254167b86f02b921cb89cde9a93d6cd
bbearcookie <aodem@naver.com>
2024-01-16 20:27:02 +0900

    ✨ Feat: EmotionTestButton 컴포넌트 및 스토리북 추가

--- src/components/EmotionTestButton.tsx
+++ src/components/EmotionTestButton.tsx
@@ -1,15 +1,16 @@
-// TODO: emotion 테스트용 컴포넌트입니다. 추후에 삭제해도 좋습니다.
-
 import React from 'react';
 import { css } from '@emotion/react';
 import clsx from 'clsx';
 
 interface EmotionTestButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  /** 버튼 안에 들어갈 내용을 지정합니다. */
   children?: React.ReactNode;
+  /** 버튼에 들어갈 패딩의 크기를 지정합니다. */
   size?: 'small' | 'medium' | 'large';
 }
 
+/** TODO: emotion 테스트용 컴포넌트입니다. 추후에 삭제해도 좋습니다. */
 const EmotionTestButton = ({
   children,
   size,
