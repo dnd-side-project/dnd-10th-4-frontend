3dbd6984482972b94a4679686fda65def577a25a
bbearcookie <aodem@naver.com>
2024-01-17 21:59:12 +0900

    🚀 deploy: Vercel 배포 팀으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ const App = () => {
   return (
     <>
       <h1>Hello World</h1>
+      <p>Vercel 배포!</p>
       <EmotionTestButton size="small">이모션 테스트</EmotionTestButton>
       <Link to={ROUTER_PATHS.TEST_CONSTANT}>Test Constant Page</Link>
       <Link to={ROUTER_PATHS.TEST_VARIABLE('5')}>Test 5 Page</Link>
