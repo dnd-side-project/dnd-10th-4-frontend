bf4cdb94f217861d2d95695b8c76c2b233681b8c
bbearcookie <aodem@naver.com>
2024-01-18 09:33:27 +0900

    📝 style: EmotionTestButton의 css를 템플릿 리터럴로 수정

--- src/components/EmotionTestButton.tsx
+++ src/components/EmotionTestButton.tsx
@@ -11,18 +11,18 @@ interface EmotionTestButtonProps
 }
 
 const styles = {
-  button: (size: 'small' | 'medium' | 'large') =>
-    css({
-      color: 'hotpink',
-      '&:hover': {
-        color: 'blue',
-      },
-      padding: clsx({
-        '1rem': size === 'small',
-        '2rem': size === 'medium',
-        '3rem': size === 'large',
-      }),
-    }),
+  button: (size: 'small' | 'medium' | 'large') => css`
+    padding: ${clsx({
+      '1rem': size === 'small',
+      '2rem': size === 'medium',
+      '3rem': size === 'large',
+    })};
+    color: hotpink;
+
+    &:hover {
+      color: blue;
+    }
+  `,
 };
 
 /** TODO: emotion 테스트용 컴포넌트입니다. 추후에 삭제해도 좋습니다. */
