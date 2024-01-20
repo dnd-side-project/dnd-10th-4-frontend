16058e06c7f40ba09b387234df60bed0dc11cd3d
bbearcookie <aodem@naver.com>
2024-01-21 01:12:26 +0900

    🚚 rename: ROUTER_PATHS 를 router 파일에 함께 정리

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import router from './router';
+import { router } from './router';
 import 'reset-css';
 
 const queryClient = new QueryClient();
