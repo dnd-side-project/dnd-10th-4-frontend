6ec6a21884b543aac18cadeb9eab9ac01e4063b0
bbearcookie <aodem@naver.com>
2024-01-23 06:12:04 +0900

    ✨ feat: KakaoLoginButton 컴포넌트 및 스토리북 작성

---
+++ src/components/KakaoLoginButton/index.tsx
@@ -0,0 +1,20 @@
+import React, { forwardRef } from 'react';
+import KakaoLogo from '@/assets/icons/kakaoLogo.svg?react';
+import styles from './styles';
+
+interface KakaoLoginButtonProps extends React.ComponentProps<'button'> {}
+
+const KakaoLoginButton = forwardRef<HTMLButtonElement, KakaoLoginButtonProps>(
+  ({ children = '카카오 로그인', ...props }, ref) => {
+    return (
+      <button ref={ref} css={styles.button} {...props}>
+        <KakaoLogo css={styles.logo} />
+        {children}
+      </button>
+    );
+  },
+);
+
+KakaoLoginButton.displayName = 'Button';
+
+export default KakaoLoginButton;
---
+++ src/components/KakaoLoginButton/styles.ts
@@ -0,0 +1,28 @@
+import { css } from '@emotion/react';
+
+const styles = {
+  button: css`
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    padding: 1rem 1.25rem;
+    border: none;
+    border-radius: 6.25rem;
+    background-color: #fee500;
+    font-weight: 700;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    cursor: pointer;
+  `,
+  logo: css`
+    position: absolute;
+    left: 0;
+    width: 1.5rem;
+    height: 1.5rem;
+    margin-left: 1.25rem;
+  `,
+};
+
+export default styles;
