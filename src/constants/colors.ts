077949161fc8864eaa9bab966153c91fe07808f1
bbearcookie <aodem@naver.com>
2024-01-30 05:54:19 +0900

    📦 chore: primary 관련 색상 상수 정리

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -6,6 +6,9 @@ const COLORS = {
   gray5: '#E0E0E0',
   gray6: '#F2F2F2',
   red: '#EB5757',
+  primary: '#0C8CE9',
+  primaryHover: '#096DB5',
+  primaryDisabled: '#C6CFD6',
 } as const;
 
 export default COLORS;
