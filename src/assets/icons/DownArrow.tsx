c3e144280df6139e27f092704e3ee9ec09542d4f
bbearcookie <aodem@naver.com>
2024-01-30 18:41:00 +0900

    💄 design: DownArrow의 stroke를 prop으로 받도록 수정

--- src/assets/icons/DownArrow.tsx
+++ src/assets/icons/DownArrow.tsx
@@ -1,14 +1,21 @@
 import type { SVGProps } from 'react';
-const SvgDownArrow = ({ ...props }: SVGProps<SVGSVGElement>) => (
+const SvgDownArrow = ({
+  width = 24,
+  height = 24,
+  stroke = 'currentColor',
+  ...props
+}: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
+    width={width}
+    height={height}
     {...props}
   >
     <g clipPath="url(#downArrow_svg__a)">
       <path
-        stroke="#BDBDBD"
+        stroke={stroke}
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth={1.5}
