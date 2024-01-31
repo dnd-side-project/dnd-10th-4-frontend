044329e36247e9db46195512444ca41528486d43
bbearcookie <aodem@naver.com>
2024-01-31 18:34:41 +0900

    🚚 rename: queryClient를 별도의 파일로 분리

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { router } from './router';
+import { QueryClientProvider } from '@tanstack/react-query';
+import queryClient from '@/api/queryClient';
+import { router } from '@/router';
 import 'reset-css';
 import './main.css';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 1,
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-    },
-  },
-});
 
 async function enableMocking() {
   if (import.meta.env.VITE_MSW !== 'on') {
