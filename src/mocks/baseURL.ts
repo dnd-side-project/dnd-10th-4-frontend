599422fe54242a6f3286e00ce172b76c86811405
bbearcookie <aodem@naver.com>
2024-02-01 01:01:46 +0900

    🚚 rename: msw 관련 유틸을 utils 디렉토리로 이동

--- src/mocks/baseURL.ts
+++
@@ -1,9 +0,0 @@
-import { BACKEND_ENDPOINT } from '@/constants/endpoint';
-
-export const baseURL = (path: string) => {
-  return BACKEND_ENDPOINT + path;
-};
-
-export type ResponseType<T> = T extends (...args: unknown[]) => Promise<infer U>
-  ? U
-  : unknown;
--- src/mocks/handlers/test.ts
+++ src/mocks/handlers/test.ts
@@ -1,29 +1,31 @@
 import { http, HttpResponse, delay } from 'msw';
 import testAPI from '@/api/test/apis';
-import { baseURL, type ResponseType } from '../baseURL';
+import { baseURL, type ResponseType } from '@/utils/mswUtils';
 
 /** @NOTE: MSW 핸들러 예시용 임시 코드입니다. */
 const testHandler = [
   http.get(baseURL('/test'), async () => {
     await delay(300);
 
-    return HttpResponse.json(
-      Array.from({ length: 5 }, (_, idx) => ({
-        id: idx.toString(),
-        name: `모킹된 ${idx} 번 데이터`,
-        content: '목 데이터를 성공적으로 가져왔습니다.',
-      })) satisfies ResponseType<(typeof testAPI)['getTestList']>,
-    );
+    const result = Array.from({ length: 5 }, (_, idx) => ({
+      id: idx.toString(),
+      name: `모킹된 ${idx} 번 데이터`,
+      content: '목 데이터를 성공적으로 가져왔습니다.',
+    })) satisfies ResponseType<(typeof testAPI)['getTestList']>;
+
+    return HttpResponse.json(result);
   }),
 
   http.get(baseURL('/test/:testId'), async (req) => {
     await delay(300);
 
-    return HttpResponse.json({
+    const result = {
       id: req.params.testId as string,
       name: `모킹된 ${req.params.testId} 번 데이터`,
       content: '목 데이터를 성공적으로 가져왔습니다.',
-    } satisfies ResponseType<(typeof testAPI)['getTestDetail']>);
+    } satisfies ResponseType<(typeof testAPI)['getTestDetail']>;
+
+    return HttpResponse.json(result);
   }),
 
   http.post(baseURL('/test'), async () => {
