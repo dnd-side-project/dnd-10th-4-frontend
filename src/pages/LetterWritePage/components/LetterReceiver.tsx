6b5ff71a3b82c829b73effe05aafd0c7e1129f29
easyhyun00 <leejihyun0324@gmail.com>
2024-02-02 01:29:20 +0900

    ✨ feat: LetterWrite 페이지 바텀시트 UI 구현

--- src/pages/LetterWritePage/components/LetterReceiver.tsx
+++ src/pages/LetterWritePage/components/LetterReceiver.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Slider from '@mui/material/Slider';
 import BottomSheet from '@/components/BottomSheet';
+import { ArrowClockWise } from '@/assets/icons';
+import COLORS from '@/constants/colors';
+import Chip from '@/components/Chip';
+import textStyles from '@/styles/textStyles';
+import style from '../styles';
 
 interface LetterReceiverProps {
   isBottomSheetOpen: boolean;
@@ -10,23 +16,75 @@ const LetterReceiver = ({
   isBottomSheetOpen,
   toggleBottomSheet,
 }: LetterReceiverProps) => {
+  const [value, setValue] = useState<number[]>([17, 39]);
+
+  const handleChange = (e: Event, newValue: number | number[]) => {
+    setValue(newValue as number[]);
+  };
+
+  const chipLabels = [
+    '일·직장',
+    '취업·진로',
+    '인간관계',
+    '이별·상실',
+    '연애',
+    '학업',
+    '가족',
+    '기타',
+  ];
+
   return (
     <BottomSheet
       open={isBottomSheetOpen}
       onClose={toggleBottomSheet(false)}
       onOpen={toggleBottomSheet(true)}
     >
-      <div>
-        <p>누구에게 보낼까요</p>
-        <div>
-          <p>나이</p>
+      <div css={{ paddingInline: '1rem' }}>
+        <div css={style.bottomSheetTitle}>
+          <h2>누구에게 보낼까요?</h2>
+          <ArrowClockWise color={COLORS.gray2} />
         </div>
         <div>
-          <p>성별</p>
+          <h3 css={style.label}>나이</h3>
+          <Slider
+            getAriaLabel={() => 'Minimum distance'}
+            value={value}
+            onChange={handleChange}
+            valueLabelDisplay="auto"
+            disableSwap
+            min={15}
+            max={50}
+            css={style.slider}
+          />
+          <div css={style.age}>
+            <p>{value[0]}세 이상</p>
+            <p>{value[1]}세 이하</p>
+          </div>
         </div>
         <div>
-          <p>고민</p>
+          <h3 css={style.label}>성별</h3>
+          <div css={style.genderChip}>
+            <Chip variant="form">모두에게 보내기</Chip>
+            <Chip variant="form">나와 같은 성별에게 보내기</Chip>
+          </div>
         </div>
+        <div>
+          <h3 css={[style.label, { paddingBottom: '4px' }]}>고민</h3>
+          <p css={textStyles.description}>
+            고민을 선택을 하면 나와 비슷한 고민을 가진 낯선 친구에게 전달되요
+          </p>
+          <div css={style.concernChip}>
+            {chipLabels.map((label) => (
+              <Chip key={label} variant="form">
+                {label}
+              </Chip>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div css={style.buttonContainer}>
+        <button>닫기</button>
+        <button>완료</button>
       </div>
     </BottomSheet>
   );
--- src/pages/LetterWritePage/styles.ts
+++ src/pages/LetterWritePage/styles.ts
@@ -77,6 +77,88 @@ const style = {
     ${textStyles.c1r}
     color: ${COLORS.gray4};
   `,
+  bottomSheetTitle: css`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 2.75rem;
+
+    h2 {
+      ${textStyles.t2}
+    }
+
+    svg {
+      cursor: pointer;
+    }
+  `,
+  label: css`
+    padding-block: 0.5rem;
+    ${textStyles.t3};
+  `,
+  slider: css`
+    color: #0c8ce9;
+
+    .MuiSlider-rail {
+      color: ${COLORS.gray5};
+    }
+
+    .MuiSlider-thumb {
+      width: 1rem;
+      height: 1rem;
+      border: 1px solid ${COLORS.gray4};
+      background: ${COLORS.gray6};
+    }
+  `,
+  age: css`
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+    color: ${COLORS.gray3};
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 24px;
+  `,
+  genderChip: css`
+    display: flex;
+    gap: 0.5rem;
+    margin-top: 0.5rem;
+    margin-bottom: 1rem;
+
+    button {
+      flex: 1;
+      color: ${COLORS.gray1};
+      font-weight: 700;
+      font-style: normal;
+      font-size: 12px;
+      line-height: 16px;
+    }
+  `,
+  concernChip: css`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.25rem;
+    margin-block: 1rem;
+  `,
+  buttonContainer: css`
+    display: flex;
+
+    button {
+      flex: 1;
+      padding: 12px 20px;
+      border: 0;
+      ${textStyles.t3}
+    }
+
+    & > button:nth-of-type(1) {
+      background-color: ${COLORS.gray5};
+      color: ${COLORS.gray1};
+    }
+
+    & > button:nth-of-type(2) {
+      background-color: ${COLORS.primary};
+      color: white;
+    }
+  `,
 };
 
 export default style;
