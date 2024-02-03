cf9c47f00b718ae68af2b7bf6595d5a555fe2ba9
bbearcookie <aodem@naver.com>
2024-02-03 23:23:24 +0900

    💄 design: Funnel 스토리북 및 설명 추가

--- src/contexts/useFunnelContext.ts
+++ src/contexts/useFunnelContext.ts
@@ -1,5 +1,6 @@
 import { createContext, useContext } from 'react';
 
+/** Funnel을 조작하는 핸들러를 하위 트리에 공유할 수 있는 Context입니다. */
 const FunnelContext = createContext({
   toNext: () => {},
   toPrev: () => {},
