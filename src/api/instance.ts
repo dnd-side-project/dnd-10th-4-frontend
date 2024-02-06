d4e040da274cfc2280f0642592123e125b35040a
bbearcookie <aodem@naver.com>
2024-02-07 01:16:08 +0900

    ✨ feat: 로그인 토큰 재발급 인터셉터 작성

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,10 +1,49 @@
-import axios from 'axios';
+import axios, { isAxiosError } from 'axios';
 import { BACKEND_ENDPOINT } from '@/constants/endpoint';
+import STORAGE_KEYS from '@/constants/storageKeys';
+import { ROUTER_PATHS } from '@/router';
+import authAPI from './auth/apis';
 
 const baseInstance = axios.create({
   baseURL: BACKEND_ENDPOINT,
   timeout: 30000,
   withCredentials: true,
 });
+
+baseInstance.interceptors.request.use((config) => {
+  config.headers.accessToken = localStorage.getItem(STORAGE_KEYS.accessToken);
+
+  return config;
+});
+
+baseInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const { config } = error;
+
+    if (isAxiosError(error)) {
+      switch (error.response?.data) {
+        case '액세스 토큰이 만료되었습니다.': {
+          const res = await authAPI.getReissue();
+          localStorage.setItem(STORAGE_KEYS.accessToken, res.accessToken);
+          localStorage.setItem(STORAGE_KEYS.refreshToken, res.refreshToken);
+          return axios(config);
+        }
+        case '리프레시 토큰이 올바르지 않습니다. 다시 로그인해주세요.': {
+          localStorage.removeItem(STORAGE_KEYS.accessToken);
+          localStorage.removeItem(STORAGE_KEYS.refreshToken);
+          window.location.href = ROUTER_PATHS.SIGNIN;
+          break;
+        }
+        default:
+          break;
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default baseInstance;
--- src/mocks/handlers/auth.ts
+++ src/mocks/handlers/auth.ts
@@ -2,10 +2,41 @@ import { http, HttpResponse, delay } from 'msw';
 import { baseURL } from '@/utils/mswUtils';
 
 const authHandler = [
-  http.post(baseURL('/oauth2/authorization/kakao'), async () => {
+  http.post(baseURL('/api/oauth2/authorization/kakao'), async () => {
     await delay(300);
 
     return HttpResponse.json();
+  }),
+
+  http.get(baseURL('/api/auth/reissue'), async (req) => {
+    await delay(300);
+
+    const refreshToken = req.request.headers.get('refreshtoken');
+
+    if (refreshToken === 'fresh') {
+      return HttpResponse.json({
+        accessToken: 'renewed',
+        refreshToken: 'renewed',
+      });
+    } else {
+      return new HttpResponse(
+        '리프레시 토큰이 올바르지 않습니다. 다시 로그인해주세요.',
+        { status: 401 },
+      );
+    }
+  }),
+
+  /** 액세스 토큰이 필요한 API를 시뮬레이션하기 위해 테스트 코드에서만 사용되는 API입니다. */
+  http.get(baseURL('/mock/auth/access-check'), async (req) => {
+    const accessToken = req.request.headers.get('accesstoken');
+
+    if (accessToken === 'fresh') {
+      return new HttpResponse('액세스 토큰이 유효합니다.');
+    } else {
+      return new HttpResponse('액세스 토큰이 만료되었습니다.', {
+        status: 401,
+      });
+    }
   }),
 ];
 
