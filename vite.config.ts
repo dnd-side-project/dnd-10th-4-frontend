5b024b58da87e6e94d247d7ee87c867cbcbc5bb8
bbearcookie <aodem@naver.com>
2024-02-06 19:34:36 +0900

    📦 chore: vitest 설치 및 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -13,5 +14,8 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  test: {
+    globals: true,
   },
 });
