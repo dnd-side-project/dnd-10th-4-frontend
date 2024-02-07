43c24d96bd451d414aa1cd3c0319c4ecc5ae5b30
bbearcookie <aodem@naver.com>
2024-02-07 18:40:45 +0900

    ✨ feat: 세션 스토리지의 isPlaying 값을 확인해서 아무 곳이나 상호작용하면 음악 On/Off 처리

--- src/components/Audio/index.tsx
+++ src/components/Audio/index.tsx
@@ -1,5 +1,6 @@
 import { useRef, useEffect } from 'react';
 import useMusicStore from '@/stores/useMusicStore';
+import useUpdateEffect from '@/hooks/useUpdateEffect';
 
 interface AudioProps {
   /** 재생할 음악의 URL입니다. */
@@ -10,15 +11,33 @@ const Audio = ({ src }: AudioProps) => {
   const audioRef = useRef<HTMLAudioElement>(null);
   const isPlaying = useMusicStore((s) => s.isPlaying);
 
+  /** 최초 렌더링시 아무 곳이나 마우스로 상호작용하면 음악 On/Off 처리 */
   useEffect(() => {
     if (audioRef.current) {
       audioRef.current.volume = 0.2;
+    }
 
-      if (isPlaying) {
+    const handleMouseUp = () => {
+      if (audioRef.current && isPlaying) {
         audioRef.current.play();
-      } else {
-        audioRef.current.pause();
+        window.removeEventListener('mouseup', handleMouseUp);
       }
+    };
+
+    window.addEventListener('mouseup', handleMouseUp);
+    return () => window.removeEventListener('mouseup', handleMouseUp);
+  }, []);
+
+  /** isPlaying 변경시 음악 On/Off 처리 */
+  useUpdateEffect(() => {
+    if (!audioRef.current) {
+      return;
+    }
+
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
     }
   }, [isPlaying]);
 
