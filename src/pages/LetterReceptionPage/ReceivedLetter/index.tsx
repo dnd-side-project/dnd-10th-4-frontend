19fd27f7a6e22a17bdad09d37f92f3fcd47f5435
easyhyun00 <leejihyun0324@gmail.com>
2024-02-08 20:26:55 +0900

    ✨ feat: ReceivedLetter 컴포넌트 구현

---
+++ src/pages/LetterReceptionPage/ReceivedLetter/index.tsx
@@ -0,0 +1,50 @@
+import { useNavigate } from 'react-router-dom';
+import { css } from '@emotion/react';
+import LetterCard from '@/components/LetterCard';
+import Navbar from '@/components/Navbar';
+import Button from '@/components/Button';
+import Tooltip from '@/components/Tooltip';
+import ReceptionHeader from '../components/ReceptionHeader';
+
+const ReceivedLetter = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <ReceptionHeader onClickBackIcon={() => navigate(-1)} />
+      <div css={style.container}>
+        <LetterCard isOpen={true}>하이</LetterCard>
+        <Navbar css={style.navbar}>
+          <Button variant="secondary" size="sm">
+            다시 흘려보내기
+          </Button>
+          <Tooltip
+            side="top"
+            delay={10000}
+            triggerContent={
+              <Button variant="primary" size="sm">
+                답장하기
+              </Button>
+            }
+          >
+            사라지기전에 답장을 보내보세요!
+          </Tooltip>
+        </Navbar>
+      </div>
+    </>
+  );
+};
+
+const style = {
+  container: css`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+    margin-inline: 0.5rem;
+  `,
+  navbar: css`
+    padding-inline: 0;
+  `,
+};
+
+export default ReceivedLetter;
