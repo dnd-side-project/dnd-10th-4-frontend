a458c8ada3b4b7ee9306e1e551eac05c8978fad5
easyhyun00 <leejihyun0324@gmail.com>
2024-02-08 14:46:47 +0900

    ✨ feat: LetterTextarea 컴포넌트에 ref  추가

--- src/components/LetterTextarea/index.tsx
+++ src/components/LetterTextarea/index.tsx
@@ -1,6 +1,8 @@
+import { forwardRef, ComponentProps } from 'react';
+import { letterWrite } from '@/constants/schemaLiteral';
 import style from './styles';
 
-interface LetterTextareaProps {
+interface LetterTextareaProps extends ComponentProps<'textarea'> {
   /** textarea 의 name 입니다. */
   name: string;
   /** textarea 의 세로 줄 개수 입니다. */
@@ -13,25 +15,33 @@ interface LetterTextareaProps {
   maxLength?: number;
 }
 
-const LetterTextarea = ({
-  name,
-  rows = 15,
-  autoFocus = true,
-  placeholder,
-  maxLength = 300,
-  ...props
-}: LetterTextareaProps) => {
-  return (
-    <textarea
-      {...props}
-      name={name}
-      rows={rows}
-      autoFocus={autoFocus}
-      placeholder={placeholder}
-      maxLength={maxLength}
-      css={style.textarea}
-    />
-  );
-};
+const LetterTextarea = forwardRef<HTMLTextAreaElement, LetterTextareaProps>(
+  (
+    {
+      name,
+      rows = 15,
+      autoFocus = true,
+      placeholder,
+      maxLength = letterWrite.content.max.value,
+      ...props
+    },
+    ref,
+  ) => {
+    return (
+      <textarea
+        {...props}
+        ref={ref}
+        name={name}
+        rows={rows}
+        autoFocus={autoFocus}
+        placeholder={placeholder}
+        maxLength={maxLength}
+        css={style.textarea}
+      />
+    );
+  },
+);
+
+LetterTextarea.displayName = 'LetterTextarea';
 
 export default LetterTextarea;
