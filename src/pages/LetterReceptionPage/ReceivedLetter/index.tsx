f9afbf68f283ab9913bf19cf53d7a6581f467a57
이지현 <98106371+easyhyun00@users.noreply.github.com>
2024-02-09 17:58:56 +0900

    ✨ feat: 흘러온 편지 페이지 UI 구현 (#83)
    * ✨ feat: ReceptionHeader 컴포넌트 구현
    
    * ✨ feat: ReceivedLetter 컴포넌트 구현
    
    * 💄 design: Letter Card, Accrodion 디자인 자연스럽게 변경
    
    * ✨ feat: ReplyToLetter 컴포넌트 구현
    
    * ✨ feat: LetterReception 페이지에 Pannel 적용
    
    * ✨ feat: 컴포넌트 전환 애니메이션 구현
    
    * 💄 design: LetterReception 스타일 적용
    
    * 🔨 refactor: LetterAccordion에useBoolean 훅 사용
    
    * 💄 design: 100svh 에서 100dvh로 변경
    
    * 🔨 refactor: 스텝 이동 핸들러 props 로 전달

---
+++ src/pages/LetterReceptionPage/ReceivedLetter/index.tsx
@@ -0,0 +1,109 @@
+import { css } from '@emotion/react';
+import LetterCard from '@/components/LetterCard';
+import Navbar from '@/components/Navbar';
+import Button from '@/components/Button';
+import Tooltip from '@/components/Tooltip';
+import Chip from '@/components/Chip';
+import textStyles from '@/styles/textStyles';
+import COLORS from '@/constants/colors';
+import LetterContent from '../components/LetterContent';
+import ReceptionPolaroid from '../components/ReceptionPolaroid';
+
+interface ReceivedLetterProps {
+  onNext: () => void;
+}
+
+const ReceivedLetter = ({ onNext }: ReceivedLetterProps) => {
+  const tagList = ['20~24세', '모두에게', '기타'];
+
+  return (
+    <LetterContent>
+      <LetterCard isOpen={true}>
+        <div css={style.from}>
+          <span>From.</span>
+          <span>낯선 고양이</span>
+        </div>
+        <div css={style.tag}>
+          {tagList.map((item) => (
+            <Chip variant="filter" key={item}>
+              # {item}
+            </Chip>
+          ))}
+        </div>
+        <div css={style.text}>
+          <p>
+            여기 거 다 남겨두고서 혹시겨두고서 혹시나 기대도 포기하려 하오 그대
+            부디 잘 지내시오 기나긴 그대 침묵을 이별로 받아두겠소 행여 이 맘
+            다칠까 근심은 접어두오 오 사랑한 사람이여 더 이상 못보아도 사실 그대
+            있음으로 힘겨운 날들을 견뎌 왔음에 감사하오 좋은 사람 만나오 사는
+            동안 날 잊고 사시오 진정 행복하길 바라겠소 이 맘만 가져가오 기나긴
+            그대 침묵을 이별로 받아두겠소 행여 이 맘 다칠까 근심은 접어두오 오
+            사랑한 사람이여 더 이상 못보아도 사실 그대 있음으로 힘겨운 날들을
+            견뎌 왔음에 감사하오 좋은 사람 만라겠소 이 맘만 가져가오
+          </p>
+        </div>
+        <div css={style.date}>
+          <span>24년 02월 08일</span>
+        </div>
+        <ReceptionPolaroid />
+      </LetterCard>
+      <Navbar css={style.navbar}>
+        <Button variant="secondary" size="sm">
+          다시 흘려보내기
+        </Button>
+        <Tooltip
+          side="top"
+          delay={10000}
+          triggerContent={
+            <Button variant="primary" size="sm" onClick={onNext}>
+              답장하기
+            </Button>
+          }
+        >
+          사라지기전에 답장을 보내보세요!
+        </Tooltip>
+      </Navbar>
+    </LetterContent>
+  );
+};
+
+const style = {
+  from: css`
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+
+    & > span:nth-of-type(1) {
+      color: ${COLORS.gray1};
+      ${textStyles.t4};
+    }
+
+    & > span:nth-of-type(2) {
+      color: ${COLORS.gray3};
+      font-weight: 500;
+      font-style: normal;
+      font-size: 14px;
+      line-height: 16px;
+    }
+  `,
+  tag: css`
+    display: flex;
+    gap: 0.5rem;
+  `,
+  text: css`
+    height: 15rem;
+    ${textStyles.l1m};
+  `,
+  date: css`
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 3.5rem;
+    ${textStyles.c1r};
+    color: ${COLORS.gray2};
+  `,
+  navbar: css`
+    padding-inline: 0;
+  `,
+};
+
+export default ReceivedLetter;
---
+++ src/pages/LetterReceptionPage/ReplyToLetter/index.tsx
@@ -0,0 +1,152 @@
+import { css } from '@emotion/react';
+import LetterCard from '@/components/LetterCard';
+import LetterAccordion from '@/components/LetterAccordion';
+import Navbar from '@/components/Navbar';
+import Button from '@/components/Button';
+import ImageUploadButton from '@/components/ImageUploadButton';
+import Chip from '@/components/Chip';
+import LetterTextarea from '@/components/LetterTextarea';
+import { letterWrite } from '@/constants/schemaLiteral';
+import { formatDate } from '@/utils/dateUtils';
+import COLORS from '@/constants/colors';
+import textStyles from '@/styles/textStyles';
+import useBoolean from '@/hooks/useBoolean';
+import LetterContent from '../components/LetterContent';
+import ReceptionPolaroid from '../components/ReceptionPolaroid';
+
+interface ReplyToLetterProps {
+  onPrev: () => void;
+}
+
+const ReplyToLetter = ({ onPrev }: ReplyToLetterProps) => {
+  const { value: isOpen, toggle: accordionToggle } = useBoolean(false);
+
+  const tagList = ['20~24세', '모두에게', '기타'];
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    console.log(file);
+  };
+
+  return (
+    <LetterContent isBlock={true}>
+      <div css={style.letter}>
+        <LetterCard css={{ marginBottom: '1rem' }}>
+          <div css={style.from}>
+            <span>From.</span>
+            <span>낯선 고양이</span>
+          </div>
+          <div css={style.tag}>
+            {tagList.map((item) => (
+              <Chip variant="filter" key={item}>
+                # {item}
+              </Chip>
+            ))}
+          </div>
+          <LetterAccordion
+            isOpen={isOpen}
+            onToggle={accordionToggle}
+            id="1"
+            text="여기까지가 사시오 진정 행복하길 바라겠소 이 맘만 가져가오 기나긴 그대 침묵을 이별로 받아두겠소 행여 이 맘 다칠까 근심은 접어두오 오 사랑한 사람이여 더 이상 못보아도 사실 그대 있음으로 힘겨운 날들을 견뎌 왔음에 감사하오 좋은 사람 만나오 사는 동안 날 잊고 사시오 진정 행복하길 바라겠소 이 맘만 가져가오여기까지가 사시오 진정 행복하길 바라겠소 이 맘만 가져가오 기나긴 그대 침묵을 이별로 받아두겠소 행여 이 맘 다칠까 근심은 접어두오 오 사랑한 사람이여 더 이상 못보아도 사실 그대 있음으로 힘겨운 날들을 견뎌 왔음에 감사하오 좋"
+            date={new Date()}
+            line={2}
+            type="send"
+          />
+          <ReceptionPolaroid />
+        </LetterCard>
+        <LetterCard isOpen={true}>
+          <div css={style.from}>
+            <span>To.</span>
+            <span>낯선 고양이</span>
+          </div>
+          <LetterTextarea
+            name="content"
+            placeholder="하고싶은 이야기를 답장으로 적어보세요."
+          />
+          <div css={style.textCount}>
+            <span>0</span>
+            <span>&nbsp;/ {letterWrite.content.max.value}</span>
+          </div>
+          <div css={style.date}>
+            <span>{formatDate(new Date())}</span>
+          </div>
+        </LetterCard>
+      </div>
+      <Navbar css={style.navbar}>
+        <Button
+          type="button"
+          variant="semi-transparent-unaccent"
+          size="sm"
+          onClick={onPrev}
+        >
+          다시 흘러보내기
+        </Button>
+        <Button type="submit" variant="semi-transparent" size="sm">
+          답장 보내기
+        </Button>
+        <ImageUploadButton onChangeImage={handleFileChange} />
+      </Navbar>
+    </LetterContent>
+  );
+};
+
+const style = {
+  from: css`
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+
+    & > span:nth-of-type(1) {
+      color: ${COLORS.gray1};
+      ${textStyles.t4};
+    }
+
+    & > span:nth-of-type(2) {
+      color: ${COLORS.gray3};
+      font-weight: 500;
+      font-style: normal;
+      font-size: 14px;
+      line-height: 16px;
+    }
+  `,
+  tag: css`
+    display: flex;
+    gap: 0.5rem;
+  `,
+  text: css`
+    ${textStyles.l1m};
+  `,
+  letter: css`
+    overflow-y: auto;
+    max-height: calc(100dvh - 60px - 80px - 16px);
+  `,
+  textCount: css`
+    display: flex;
+    justify-content: flex-end;
+
+    ${textStyles.c1m}
+    & > span:nth-of-type(1) {
+      color: ${COLORS.gray1};
+    }
+
+    & > span:nth-of-type(2) {
+      color: ${COLORS.gray4};
+    }
+  `,
+  date: css`
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 2.5rem;
+    ${textStyles.c1r}
+    color: ${COLORS.gray4};
+  `,
+  navbar: css`
+    position: fixed;
+    bottom: 0;
+    width: 96vw;
+    max-width: 580px;
+    padding-inline: 0;
+  `,
+};
+
+export default ReplyToLetter;
