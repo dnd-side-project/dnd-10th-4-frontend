aed8eb5c2d582412f2e99f927c06fb1bd2ddf972
bbearcookie <aodem@naver.com>
2024-02-10 01:23:20 +0900

    💄 design: 배경 이미지 움직이기

--- src/components/Background/styles.ts
+++ src/components/Background/styles.ts
@@ -2,18 +2,53 @@ import { css } from '@emotion/react';
 
 const style = {
   background: (imageUrl: string) => css`
+    @keyframes gradient {
+      0% {
+        background-position: 0% center;
+      }
+
+      100% {
+        background-position: 36000% center;
+      }
+    }
+
     height: 100svh;
     background-image: url(${imageUrl});
     background-position: center center;
-    background-size: 37.5rem 100%;
+    background-size: 600px 100%;
     background-repeat: repeat-x;
+    animation: gradient linear infinite;
+    animation-duration: 10000s;
+
+    @media (max-width: 1200px) {
+      animation-duration: 7200s;
+    }
+
+    @media (max-width: 1000px) {
+      animation-duration: 4800s;
+    }
+
+    @media (max-width: 900px) {
+      animation-duration: 4000s;
+    }
+
+    @media (max-width: 800px) {
+      animation-duration: 3600s;
+    }
+
+    @media (max-width: 700px) {
+      animation-duration: 2400s;
+    }
+
+    @media (max-width: 650px) {
+      animation-duration: 1600s;
+    }
 
     @media (max-width: 600px) {
-      background-position: center;
-      background-size: cover;
-      background-repeat: no-repeat;
+      animation-duration: 1200s;
     }
   `,
+
   wrapper: css`
     display: flex;
     flex-direction: column;
