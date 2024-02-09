418d19412dedec2ebba5dabae90965d3e6d01735
bbearcookie <aodem@naver.com>
2024-02-09 19:16:13 +0900

    ✨ feat: 받은 편지, 받은 답장 API 함수 및 모킹 작성

--- src/constants/queryStrings.ts
+++ src/constants/queryStrings.ts
@@ -1,5 +1,6 @@
 const QUERY_STRINGS = {
   mswCase: 'mswCase',
+  mswItemCount: 'mswItemCount',
 } as const;
 
 export default QUERY_STRINGS;
--- src/mocks/handlers/letter.ts
+++ src/mocks/handlers/letter.ts
@@ -1,7 +1,46 @@
 import { http, HttpResponse, delay } from 'msw';
 import { baseURL } from '@/utils/mswUtils';
+import QUERY_STRINGS from '@/constants/queryStrings';
+import {
+  ReceivedLetterResponse,
+  PagedRepliedLettersResponse,
+} from '../datas/letter';
 
 const letterHandler = [
+  http.get(baseURL('/api/letter/reception'), async () => {
+    await delay(1000);
+
+    const itemCount =
+      Number(
+        new URLSearchParams(window.location.search).get(
+          QUERY_STRINGS.mswItemCount,
+        ),
+      ) || ReceivedLetterResponse.length;
+
+    return HttpResponse.json(
+      ReceivedLetterResponse.slice(0, Number(itemCount)),
+    );
+  }),
+
+  http.get(baseURL('/api/letter/reply'), async () => {
+    await delay(1000);
+
+    const itemCount =
+      Number(
+        new URLSearchParams(window.location.search).get(
+          QUERY_STRINGS.mswItemCount,
+        ),
+      ) || ReceivedLetterResponse.length;
+
+    return HttpResponse.json({
+      ...PagedRepliedLettersResponse,
+      postList: PagedRepliedLettersResponse.postList.slice(
+        0,
+        Number(itemCount),
+      ),
+    });
+  }),
+
   http.post(baseURL('/api/letter'), async () => {
     await delay(1000);
     return HttpResponse.json();
