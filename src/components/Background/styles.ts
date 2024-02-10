fc3a2acfca7629b2d529627397691b60cd6d2715
SangHoon Lee <50488780+bbearcookie@users.noreply.github.com>
2024-02-11 03:24:41 +0900

    💄 design: 배경 이미지 움직이기 (#87)
    * 💄 design: 배경 이미지 움직이기
    
    * 💄 design: 배경 이동 속도 더 느리게 조정

--- src/components/Background/styles.ts
+++ src/components/Background/styles.ts
@@ -2,18 +2,57 @@ import { css } from '@emotion/react';
 
 const style = {
   background: (imageUrl: string) => css`
+    @keyframes gradient {
+      0% {
+        background-position: 0% center;
+      }
+
+      100% {
+        background-position: 36000% center;
+      }
+    }
+
     height: 100svh;
     background-image: url(${imageUrl});
     background-position: center center;
-    background-size: 37.5rem 100%;
+    background-size: 600px 100%;
     background-repeat: repeat-x;
+    animation: gradient linear infinite;
+    animation-duration: 20000s;
+
+    @media (max-width: 1200px) {
+      animation-duration: 14400s;
+    }
+
+    @media (max-width: 1000px) {
+      animation-duration: 9600s;
+    }
+
+    @media (max-width: 900px) {
+      animation-duration: 8000s;
+    }
+
+    @media (max-width: 800px) {
+      animation-duration: 7200s;
+    }
+
+    @media (max-width: 700px) {
+      animation-duration: 4800s;
+    }
+
+    @media (max-width: 650px) {
+      animation-duration: 3200s;
+    }
 
     @media (max-width: 600px) {
-      background-position: center;
-      background-size: cover;
-      background-repeat: no-repeat;
+      animation-duration: 2400s;
+    }
+
+    @media (max-width: 400px) {
+      animation-duration: 10000s;
     }
   `,
+
   wrapper: css`
     display: flex;
     flex-direction: column;
