58b60a1f0b67205f88e98c83dab15736cbcb85f1
bbearcookie <aodem@naver.com>
2024-02-11 21:49:42 +0900

    🔨 refactor: 온보딩 페이지 컴포넌트에 훅폼 적용

--- src/pages/OnboardingPage/index.tsx
+++ src/pages/OnboardingPage/index.tsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
+import {
+  FormProvider,
+  SubmitHandler,
+  SubmitErrorHandler,
+} from 'react-hook-form';
+// import { useNavigate } from 'react-router-dom';
 import createFunnel from '@/components/Funnel/createFunnel';
 import { FunnelProvider } from '@/contexts/useFunnelContext';
 import Header from '@/components/Header';
 import MusicButton from '@/components/MusicButton';
+// import { ROUTER_PATHS } from '@/router';
 import FirstStep from './steps/FirstStep';
 import IntroduceStep from './steps/IntroduceStep';
 import InputNicknameStep from './steps/InputNicknameStep';
@@ -13,6 +20,7 @@ import InputWorryStep from './steps/InputWorryStep';
 import LastStep from './steps/LastStep';
 import NavHeader from './components/NavHeader';
 import styles from './styles';
+import useOnboardingForm, { Inputs } from './hooks/useOnboardingForm';
 
 const { Funnel, Step, useFunnel } = createFunnel([
   'First',
@@ -27,6 +35,7 @@ const { Funnel, Step, useFunnel } = createFunnel([
 
 const OnboardingPage = () => {
   const { step, toPrev, toNext, toFirst, toLast } = useFunnel();
+  // const navigate = useNavigate();
 
   const [progress, setProgress] = useState<
     React.ComponentProps<typeof NavHeader>
@@ -36,92 +45,110 @@ const OnboardingPage = () => {
     showSkipButton: false,
   });
 
+  const form = useOnboardingForm();
+  const { trigger, handleSubmit } = form;
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
+  const onError: SubmitErrorHandler<Inputs> = (errors) => {
+    // TODO: 스텝 건너뛰기해서 유효성 검사 안된거 있으면 API 요청 안하고 바로 메인으로 가도록 하기
+    console.error(errors);
+  };
+
   return (
     <FunnelProvider value={{ toPrev, toNext, toFirst, toLast }}>
-      <div css={styles.container}>
+      <form css={styles.container} onSubmit={handleSubmit(onSubmit, onError)}>
         <NavHeader {...progress} />
         <Header rightContent={<MusicButton />} />
-        <Funnel step={step}>
-          <Step
-            name="First"
-            onEnter={() =>
-              setProgress({
-                progressValue: 0,
-                showBackButton: false,
-                showSkipButton: false,
-              })
-            }
-          >
-            <FirstStep />
-          </Step>
-          <Step name="Introduce">
-            <IntroduceStep />
-          </Step>
-          <Step
-            name="Input_Nickname"
-            onEnter={() =>
-              setProgress({
-                progressValue: 1,
-                showBackButton: true,
-                showSkipButton: false,
-              })
-            }
-          >
-            <InputNicknameStep />
-          </Step>
-          <Step name="Show_Nickname">
-            <ShowNicknameStep />
-          </Step>
-          <Step
-            name="Input_Birthday"
-            onEnter={() =>
-              setProgress({
-                progressValue: 2,
-                showBackButton: true,
-                showSkipButton: true,
-              })
-            }
-          >
-            <InputBirthdayStep />
-          </Step>
-          <Step
-            name="Input_Gender"
-            onEnter={() =>
-              setProgress({
-                progressValue: 3,
-                showBackButton: true,
-                showSkipButton: true,
-              })
-            }
-          >
-            <InputGenderStep />
-          </Step>
-          <Step
-            name="Input_Worry"
-            onEnter={() =>
-              setProgress({
-                progressValue: 4,
-                showBackButton: true,
-                showSkipButton: true,
-              })
-            }
-          >
-            <InputWorryStep />
-          </Step>
-          <Step
-            name="Last"
-            onEnter={() =>
-              setProgress({
-                progressValue: 0,
-                showBackButton: true,
-                showSkipButton: false,
-              })
-            }
-          >
-            <LastStep />
-          </Step>
-        </Funnel>
-      </div>
+
+        <FormProvider {...form}>
+          <Funnel step={step}>
+            <Step
+              name="First"
+              onEnter={() =>
+                setProgress({
+                  progressValue: 0,
+                  showBackButton: false,
+                  showSkipButton: false,
+                })
+              }
+            >
+              <FirstStep />
+            </Step>
+            <Step name="Introduce">
+              <IntroduceStep />
+            </Step>
+            <Step
+              name="Input_Nickname"
+              onEnter={() =>
+                setProgress({
+                  progressValue: 1,
+                  showBackButton: true,
+                  showSkipButton: false,
+                })
+              }
+            >
+              <InputNicknameStep />
+            </Step>
+            <Step name="Show_Nickname">
+              <ShowNicknameStep />
+            </Step>
+            <Step
+              name="Input_Birthday"
+              onEnter={() => {
+                setProgress({
+                  progressValue: 2,
+                  showBackButton: true,
+                  showSkipButton: true,
+                });
+                trigger('birthday');
+              }}
+            >
+              <InputBirthdayStep />
+            </Step>
+            <Step
+              name="Input_Gender"
+              onEnter={() => {
+                setProgress({
+                  progressValue: 3,
+                  showBackButton: true,
+                  showSkipButton: true,
+                });
+                trigger('gender');
+              }}
+            >
+              <InputGenderStep />
+            </Step>
+            <Step
+              name="Input_Worry"
+              onEnter={() => {
+                setProgress({
+                  progressValue: 4,
+                  showBackButton: true,
+                  showSkipButton: true,
+                });
+                trigger('worries');
+              }}
+            >
+              <InputWorryStep />
+            </Step>
+            <Step
+              name="Last"
+              onEnter={() =>
+                setProgress({
+                  progressValue: 0,
+                  showBackButton: true,
+                  showSkipButton: false,
+                })
+              }
+            >
+              <LastStep />
+            </Step>
+          </Funnel>
+        </FormProvider>
+      </form>
     </FunnelProvider>
   );
 };
