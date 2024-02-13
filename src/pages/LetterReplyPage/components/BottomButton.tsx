dd62f80a854e6e50bb35e10279b5396968eb6bc1
easyhyun00 <leejihyun0324@gmail.com>
2024-02-13 23:27:43 +0900

    💄 design: 화살표 cursor pointer 설정

--- src/pages/LetterReplyPage/components/BottomButton.tsx
+++ src/pages/LetterReplyPage/components/BottomButton.tsx
@@ -1,12 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import { css } from '@emotion/react';
 import Navbar from '@/components/Navbar';
 import Button from '@/components/Button';
 import { TreasureChestOutline } from '@/assets/icons';
+import { ROUTER_PATHS } from '@/router';
 
 const BottomButton = () => {
+  const navigate = useNavigate();
   return (
     <Navbar css={styles.navbar}>
-      <Button variant="semi-transparent-unaccent" size="sm">
+      <Button
+        variant="semi-transparent-unaccent"
+        size="sm"
+        onClick={() => navigate(ROUTER_PATHS.ROOT)}
+      >
         닫기
       </Button>
       <Button variant="semi-transparent" size="sm">
