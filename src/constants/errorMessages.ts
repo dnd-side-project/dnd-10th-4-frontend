eb165b4efeb950ac4f8ddbe9694e241d8f1eb246
bbearcookie <aodem@naver.com>
2024-02-17 18:24:23 +0900

    🚑 fix: authenticationEntryPoint 예외 발생시 로그인 페이지로 이동

--- src/constants/errorMessages.ts
+++ src/constants/errorMessages.ts
@@ -1,4 +1,5 @@
 const ERROR_RESPONSES = {
+  authenticationEntryPoint: '로그인이 필요한 요청 입니다.',
   accessExpired: '액세스 토큰이 만료되었습니다.',
   reissueFailed: '리프레시 토큰이 올바르지 않습니다. 다시 로그인해주세요.',
   usernameNotFound: '존재하지 않는 회원입니다.',
