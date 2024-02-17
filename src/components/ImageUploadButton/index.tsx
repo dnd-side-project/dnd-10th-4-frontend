de2094c634c8d921d7bd2fd402be7f5af1321b7c
easyhyun00 <leejihyun0324@gmail.com>
2024-02-17 14:53:58 +0900

    ✨ feat: ImageUploadButton 버튼을 폴라로이드로 변경

--- src/components/ImageUploadButton/index.tsx
+++ src/components/ImageUploadButton/index.tsx
@@ -1,16 +1,21 @@
 import { useRef } from 'react';
-import Button from '@/components/Button';
-import { ImageSquare } from '@/assets/icons';
+import Polaroid from '../Polaroid';
 import style from './styles';
 
 interface ImageUploadButtonProps {
   onChangeImage: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  topPosition: number;
+  leftPosition: number;
 }
 
-const ImageUploadButton = ({ onChangeImage }: ImageUploadButtonProps) => {
+const ImageUploadButton = ({
+  onChangeImage,
+  topPosition,
+  leftPosition,
+}: ImageUploadButtonProps) => {
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-  const handleButtonClick = () => {
+  const handlePolaroidClick = () => {
     if (fileInputRef.current) {
       fileInputRef.current.click();
     }
@@ -25,15 +30,11 @@ const ImageUploadButton = ({ onChangeImage }: ImageUploadButtonProps) => {
         css={style.input}
         onChange={onChangeImage}
       />
-      <Button
-        type="button"
-        css={style.iconContainer}
-        variant="semi-transparent"
-        size="sm"
-        onClick={handleButtonClick}
-      >
-        <ImageSquare width={24} />
-      </Button>
+      <Polaroid
+        onClick={handlePolaroidClick}
+        topPosition={topPosition}
+        leftPosition={leftPosition}
+      />
     </>
   );
 };
