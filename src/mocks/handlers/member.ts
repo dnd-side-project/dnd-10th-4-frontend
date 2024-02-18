1982e5e895ece8ba07ce3fec3e0c7e163dfd4abd
bbearcookie <aodem@naver.com>
2024-02-18 22:36:51 +0900

    🔨 refactor: member 관련 핸들러에 withAuth 적용

--- src/mocks/handlers/member.ts
+++ src/mocks/handlers/member.ts
@@ -3,97 +3,119 @@ import memberAPI from '@/api/member/apis';
 import { baseURL, getSearchParams } from '@/utils/mswUtils';
 import QUERY_STRINGS from '@/constants/queryStrings';
 import ERROR_RESPONSES from '@/constants/errorMessages';
+import withAuth from '../middlewares/withAuth';
 
 const memberHandler = [
-  http.get(baseURL('/api/member'), async () => {
-    await delay(300);
+  http.get(
+    baseURL('/api/member'),
+    withAuth(async () => {
+      await delay(300);
 
-    const status: number = 200;
-    const mswCase = getSearchParams('mswCase');
+      const status: number = 200;
+      const mswCase = getSearchParams('mswCase');
 
-    switch (status) {
-      case 200:
-        if (mswCase === 'empty') {
-          return HttpResponse.json({
-            id: 8,
-            email: 'aodem@naver.com',
-            nickname: null,
-            worryTypes: [],
-            gender: null,
-            birthDay: null,
-            age: null,
-            role: 'USER',
-          } satisfies Awaited<
-            ReturnType<(typeof memberAPI)['getMemberDetail']>
-          >);
-        } else {
-          return HttpResponse.json({
-            id: 7,
-            email: 'tnlswodnjs99@naver.com',
-            nickname: '낯선 거북이',
-            worryTypes: ['COURSE', 'STUDY', 'RELATIONSHIP'],
-            gender: 'MALE',
-            birthDay: [1997, 1, 1],
-            age: 28,
-            role: 'USER',
-          } satisfies Awaited<
-            ReturnType<(typeof memberAPI)['getMemberDetail']>
-          >);
-        }
-      case 404:
+      switch (status) {
+        case 200:
+          if (mswCase === 'empty') {
+            return HttpResponse.json({
+              id: 8,
+              email: 'aodem@naver.com',
+              nickname: null,
+              worryTypes: [],
+              gender: null,
+              birthDay: null,
+              age: null,
+              role: 'USER',
+            } satisfies Awaited<
+              ReturnType<(typeof memberAPI)['getMemberDetail']>
+            >);
+          } else {
+            return HttpResponse.json({
+              id: 7,
+              email: 'tnlswodnjs99@naver.com',
+              nickname: '낯선 거북이',
+              worryTypes: ['COURSE', 'STUDY', 'RELATIONSHIP'],
+              gender: 'MALE',
+              birthDay: [1997, 1, 1],
+              age: 28,
+              role: 'USER',
+            } satisfies Awaited<
+              ReturnType<(typeof memberAPI)['getMemberDetail']>
+            >);
+          }
+        case 404:
+          return new HttpResponse(ERROR_RESPONSES.memberNotFound, {
+            status: 404,
+          });
+        default:
+          return;
+      }
+    }),
+  ),
+
+  http.patch(
+    baseURL('/api/member'),
+    withAuth(async () => {
+      await delay(1000);
+
+      const mswCase = new URLSearchParams(window.location.search).get(
+        QUERY_STRINGS.mswCase,
+      );
+
+      if (mswCase === '404') {
         return new HttpResponse(ERROR_RESPONSES.memberNotFound, {
           status: 404,
         });
-      default:
-        return;
-    }
-  }),
+      }
 
-  http.patch(baseURL('/api/member'), async () => {
-    await delay(1000);
+      return HttpResponse.json();
+    }),
+  ),
 
-    const mswCase = new URLSearchParams(window.location.search).get(
-      QUERY_STRINGS.mswCase,
-    );
+  http.patch(
+    baseURL('/api/member/nickname'),
+    withAuth(async () => {
+      await delay(1000);
 
-    if (mswCase === '404') {
-      return new HttpResponse(ERROR_RESPONSES.memberNotFound, {
-        status: 404,
-      });
-    }
+      return HttpResponse.json();
+    }),
+  ),
 
-    return HttpResponse.json();
-  }),
+  http.patch(
+    baseURL('/api/member/birthday'),
+    withAuth(async () => {
+      await delay(1000);
 
-  http.patch(baseURL('/api/member/nickname'), async () => {
-    await delay(1000);
+      return HttpResponse.json();
+    }),
+  ),
 
-    return HttpResponse.json();
-  }),
+  http.patch(
+    baseURL('/api/member/gender'),
+    withAuth(async () => {
+      await delay(1000);
 
-  http.patch(baseURL('/api/member/birthday'), async () => {
-    await delay(1000);
+      return HttpResponse.json();
+    }),
+  ),
 
-    return HttpResponse.json();
-  }),
+  http.delete(
+    baseURL('/api/member/worry'),
+    withAuth(async () => {
+      await delay(1000);
 
-  http.patch(baseURL('/api/member/gender'), async () => {
-    await delay(1000);
+      return HttpResponse.json();
+    }),
+  ),
 
-    return HttpResponse.json();
-  }),
+  http.post(
+    baseURL('/api/member/worry'),
+    withAuth(async () => {
+      await delay(1000);
 
-  http.delete(baseURL('/api/member/worry'), async () => {
-    await delay(1000);
-
-    return HttpResponse.json();
-  }),
-
-  http.post(baseURL('/api/member/worry'), async () => {
-    await delay(1000);
-
-    return HttpResponse.json();
-  }),
+      return HttpResponse.json();
+    }),
+  ),
 ];
 
 export default memberHandler;
