5cc144d8bc4e779902f27b8c724679812154f4ae
bbearcookie <aodem@naver.com>
2024-02-18 21:27:05 +0900

    🔨 refactor: letter 관련 핸들러 switch 문으로 분기처리

--- src/mocks/handlers/letter.ts
+++ src/mocks/handlers/letter.ts
@@ -1,74 +1,63 @@
 import { http, HttpResponse, delay } from 'msw';
-import { baseURL, isValidToken } from '@/utils/mswUtils';
-import QUERY_STRINGS from '@/constants/queryStrings';
+import { baseURL } from '@/utils/mswUtils';
 import { Reception, Reply } from '@/types/letter';
 import ERROR_RESPONSES from '@/constants/errorMessages';
-import STORAGE_KEYS from '@/constants/storageKeys';
 import {
   ReceivedLetterResponse,
   RepliedLettersResponse,
 } from '../datas/letter';
 
 const letterHandler = [
-  http.get(baseURL('/api/letter/reception'), async (req) => {
-    const accessToken = req.request.headers.get(STORAGE_KEYS.accessToken);
-
+  http.get(baseURL('/api/letter/reception'), async () => {
     await delay(1000);
 
     const itemCount =
-      Number(
-        new URLSearchParams(window.location.search).get(
-          QUERY_STRINGS.mswItemCount,
-        ),
-      ) || ReceivedLetterResponse.length;
+      Number(new URLSearchParams(window.location.search).get('mswItemCount')) ||
+      ReceivedLetterResponse.length;
 
-    if (isValidToken(accessToken)) {
-      return HttpResponse.json(
-        ReceivedLetterResponse.slice(0, Number(itemCount)),
-      );
-    } else {
-      return new HttpResponse(ERROR_RESPONSES.accessExpired, { status: 401 });
+    const status: number = 200;
+
+    switch (status) {
+      case 200:
+        return HttpResponse.json(
+          ReceivedLetterResponse.slice(0, Number(itemCount)),
+        );
+      case 401:
+        return new HttpResponse(ERROR_RESPONSES.accessExpired, { status });
+      default:
+        break;
     }
   }),
 
-  http.get(baseURL('/api/letter/reply'), async (req) => {
-    const accessToken = req.request.headers.get(STORAGE_KEYS.accessToken);
-
+  http.get(baseURL('/api/letter/reply'), async () => {
     await delay(1000);
 
     const itemCount =
-      Number(
-        new URLSearchParams(window.location.search).get(
-          QUERY_STRINGS.mswItemCount,
-        ),
-      ) || RepliedLettersResponse.length;
+      Number(new URLSearchParams(window.location.search).get('mswItemCount')) ||
+      ReceivedLetterResponse.length;
 
-    if (isValidToken(accessToken)) {
-      return HttpResponse.json(
-        RepliedLettersResponse.slice(0, Number(itemCount)),
-      );
-    } else {
-      return new HttpResponse(ERROR_RESPONSES.accessExpired, { status: 401 });
+    const status: number = 200;
+
+    switch (status) {
+      case 200:
+        return HttpResponse.json(
+          RepliedLettersResponse.slice(0, Number(itemCount)),
+        );
+      case 401:
+        return new HttpResponse(ERROR_RESPONSES.accessExpired, { status });
+      default:
+        break;
     }
   }),
 
   http.post(baseURL('/api/letter'), async () => {
     await delay(1000);
+
     return HttpResponse.json();
   }),
 
   http.get(baseURL('/api/letter/reception/:letterId'), async (req) => {
     await delay(300);
-
-    const mswCase = new URLSearchParams(window.location.search).get(
-      QUERY_STRINGS.mswCase,
-    );
-
-    if (mswCase === '400') {
-      return new HttpResponse(ERROR_RESPONSES.accessDeniedLetter, {
-        status: 400,
-      });
-    }
 
     const result: Reception = {
       createdAt: '2024-02-13T16:50:44',
@@ -78,6 +67,19 @@ const letterHandler = [
       content: `${req.params.letterId} 번째 편지 입니다. 여기 거 다 남겨두고서 혹시겨두고서 혹시나 기대도 포기하려 하오 그대 부디 잘 지내시오 기나긴 그대 침묵을 이별로 받아두겠소 행여 이 맘 다칠까 근심은 접어두오 오 사랑한 사람이여 더 이상 못보아도 사실 그대 있음으로 힘겨운 날들을 견뎌 왔음에 감사하오 좋은 사람 만나오 사는 동안 날 잊고 사시오 진정 행복하길 바라겠소 이 맘만 가져가오 기나긴 그대 침묵을 이별로 받아두겠소 행여 이 맘 다칠까 근심은 접어두오 오 사랑한 사람이여 더 이상 못보아도 사실 그대 있음으로 힘겨운 날들을 견뎌 왔음에 감사하오`,
       worryType: 'BREAK_LOVE',
     };
+
+    const status: number = 200;
+
+    switch (status) {
+      case 200:
+        return HttpResponse.json(result, {
+          status,
+        });
+      case 400:
+        return new HttpResponse(ERROR_RESPONSES.accessDeniedLetter, { status });
+      default:
+        break;
+    }
 
     return HttpResponse.json(result);
   }),
@@ -107,7 +109,14 @@ const letterHandler = [
       worryType: 'BREAK_LOVE',
     };
 
-    return HttpResponse.json(result);
+    const status: number = 200;
+
+    switch (status) {
+      case 200:
+        return HttpResponse.json(result);
+      default:
+        break;
+    }
   }),
 
   http.patch(baseURL('/api/letter/reception/storage/:letterId'), async () => {
