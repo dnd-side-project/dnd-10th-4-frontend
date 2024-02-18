5aa4ca7c094a7576ffbe6cb86ba8e96eb0b9affe
SangHoon Lee <50488780+bbearcookie@users.noreply.github.com>
2024-02-18 18:28:36 +0900

    📦 chore: react-toastify 설치 (#133)
    * 📦 chore: react-toastify 설치
    
    * ✨ feat: 온보딩 페이지 토스트 적용

--- src/constants/errorMessages.ts
+++ src/constants/errorMessages.ts
@@ -1,4 +1,5 @@
 const ERROR_RESPONSES = {
+  unknown: '알 수 없는 오류가 발생했습니다.',
   authenticationEntryPoint: '로그인이 필요한 요청 입니다.',
   accessExpired: '액세스 토큰이 만료되었습니다.',
   reissueFailed: '리프레시 토큰이 올바르지 않습니다. 다시 로그인해주세요.',
