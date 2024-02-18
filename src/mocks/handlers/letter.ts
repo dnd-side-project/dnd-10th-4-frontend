fef73902bf93012862521be85c9918e920a6e87d
bbearcookie <aodem@naver.com>
2024-02-18 22:46:33 +0900

    ➖ remove: 분기 처리 불필요한 곳 status 제거

--- src/mocks/handlers/letter.ts
+++ src/mocks/handlers/letter.ts
@@ -13,19 +13,13 @@ const letterHandler = [
     withAuth(async () => {
       await delay(1000);
 
-      const status: number = 200;
       const itemCount =
         Number(getSearchParams('mswItemCount')) ||
         ReceivedLetterResponse.length;
 
-      switch (status) {
-        case 200:
-          return HttpResponse.json(
-            ReceivedLetterResponse.slice(0, Number(itemCount)),
-          );
-        default:
-          break;
-      }
+      return HttpResponse.json(
+        ReceivedLetterResponse.slice(0, Number(itemCount)),
+      );
     }),
   ),
 
@@ -34,19 +28,13 @@ const letterHandler = [
     withAuth(async () => {
       await delay(1000);
 
-      const status: number = 200;
       const itemCount =
         Number(getSearchParams('mswItemCount')) ||
         RepliedLettersResponse.length;
 
-      switch (status) {
-        case 200:
-          return HttpResponse.json(
-            RepliedLettersResponse.slice(0, Number(itemCount)),
-          );
-        default:
-          break;
-      }
+      return HttpResponse.json(
+        RepliedLettersResponse.slice(0, Number(itemCount)),
+      );
     }),
   ),
 
@@ -64,7 +52,6 @@ const letterHandler = [
     withAuth(async (req) => {
       await delay(300);
 
-      const status: number = 200;
       const result: Reception = {
         createdAt: '2024-02-13T16:50:44',
         letterId: Number(req.params.letterId),
@@ -73,15 +60,6 @@ const letterHandler = [
         content: `${req.params.letterId} 번째 편지 입니다. 여기 거 다 남겨두고서 혹시겨두고서 혹시나 기대도 포기하려 하오 그대 부디 잘 지내시오 기나긴 그대 침묵을 이별로 받아두겠소 행여 이 맘 다칠까 근심은 접어두오 오 사랑한 사람이여 더 이상 못보아도 사실 그대 있음으로 힘겨운 날들을 견뎌 왔음에 감사하오 좋은 사람 만나오 사는 동안 날 잊고 사시오 진정 행복하길 바라겠소 이 맘만 가져가오 기나긴 그대 침묵을 이별로 받아두겠소 행여 이 맘 다칠까 근심은 접어두오 오 사랑한 사람이여 더 이상 못보아도 사실 그대 있음으로 힘겨운 날들을 견뎌 왔음에 감사하오`,
         worryType: 'BREAK_LOVE',
       };
-
-      switch (status) {
-        case 200:
-          return HttpResponse.json(result, {
-            status,
-          });
-        default:
-          break;
-      }
 
       return HttpResponse.json(result);
     }),
@@ -110,7 +88,6 @@ const letterHandler = [
     withAuth(async (req) => {
       await delay(300);
 
-      const status: number = 200;
       const result: Reply = {
         createdAt: '2024-02-13T16:50:44',
         letterId: Number(req.params.letterId),
@@ -121,12 +98,7 @@ const letterHandler = [
         worryType: 'BREAK_LOVE',
       };
 
-      switch (status) {
-        case 200:
-          return HttpResponse.json(result);
-        default:
-          break;
-      }
+      return HttpResponse.json(result);
     }),
   ),
 
