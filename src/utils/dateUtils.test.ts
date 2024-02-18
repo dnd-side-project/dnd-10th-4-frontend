457e69522573d37db22469669ba22bd4fe211958
easyhyun00 <leejihyun0324@gmail.com>
2024-02-18 09:54:59 +0900

    🔨 refactor: 디테일 시간 문자열 변수명 변경

--- src/utils/dateUtils.test.ts
+++ src/utils/dateUtils.test.ts
@@ -151,41 +151,41 @@ describe('getTimeDifference', () => {
 });
 
 describe('formatDetailTimechipDate', () => {
-  it('두 날짜의 차이를 TimeChip에서 사용되는 문자열 형태로 반환해야 한다', () => {
+  it('두 날짜의 차이를 흘러온 편지, 내게 온 편지 TimeChip에서 사용되는 문자열 형태로 반환해야 한다', () => {
     expect(
       formatDetailTimechipDate(
         new Date('2021-01-01T00:00:00'),
         new Date('2021-01-01T00:01:00'),
       ),
-    ).toEqual({ expiredAt: '00:01', isAlmostExpired: true });
+    ).toEqual({ timeText: '00:01', isAlmostExpired: true });
 
     expect(
       formatDetailTimechipDate(
         new Date('2021-01-01T00:00:00'),
         new Date('2021-01-01T00:59:00'),
       ),
-    ).toEqual({ expiredAt: '00:59', isAlmostExpired: true });
+    ).toEqual({ timeText: '00:59', isAlmostExpired: true });
 
     expect(
       formatDetailTimechipDate(
         new Date('2021-01-01T00:00:00'),
         new Date('2021-01-01T01:01:00'),
       ),
-    ).toEqual({ expiredAt: '01:01', isAlmostExpired: false });
+    ).toEqual({ timeText: '01:01', isAlmostExpired: false });
 
     expect(
       formatDetailTimechipDate(
         new Date('2021-01-01T00:00:00'),
         new Date('2021-01-01T05:30:00'),
       ),
-    ).toEqual({ expiredAt: '05:30', isAlmostExpired: false });
+    ).toEqual({ timeText: '05:30', isAlmostExpired: false });
 
     expect(
       formatDetailTimechipDate(
         new Date('2021-01-01T00:00:00'),
         new Date('2021-01-03T00:00:00'),
       ),
-    ).toEqual({ expiredAt: '48:00', isAlmostExpired: false });
+    ).toEqual({ timeText: '48:00', isAlmostExpired: false });
   });
 
   it('유효하지 않은 Date 객체가 들어오면 에러를 반환해야 한다', () => {
--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -21,16 +21,16 @@ const formatTimechipDate = (from: Date, to: Date): string => {
 const formatDetailTimechipDate = (
   from: Date,
   to: Date,
-): { expiredAt: string; isAlmostExpired: boolean } => {
+): { timeText: string; isAlmostExpired: boolean } => {
   const { minute, hour } = getTimeDifference(from, to);
 
   const formattedHour = hour.toString().padStart(2, '0');
   const formattedMinute = (minute - hour * 60).toString().padStart(2, '0');
 
   const isAlmostExpired = hour === 0;
-  const expiredAt = `${formattedHour}:${formattedMinute}`;
+  const timeText = `${formattedHour}:${formattedMinute}`;
 
-  return { expiredAt, isAlmostExpired };
+  return { timeText, isAlmostExpired };
 };
 
 /** Date 객체가 정상적인지 확인 */
