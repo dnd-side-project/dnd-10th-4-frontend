cd3f27fad4091ffc893122ab3f5b364e3b0bce28
easyhyun00 <leejihyun0324@gmail.com>
2024-02-23 16:18:19 +0900

    ✨ feat: 하단 취소 버튼 바텀시트 적용

--- src/pages/LetterWritePage/components/LetterWriteBottom.tsx
+++ src/pages/LetterWritePage/components/LetterWriteBottom.tsx
@@ -1,9 +1,12 @@
 import { useNavigate } from 'react-router-dom';
+import { useFormContext } from 'react-hook-form';
 import { css } from '@emotion/react';
 import Navbar from '@/components/Navbar';
 import Button from '@/components/Button';
-import { ROUTER_PATHS } from '@/router';
 import LoadingSpinner from '@/components/LoadingSpinner';
+import useBoolean from '@/hooks/useBoolean';
+import BottomSheet from '@/components/BottomSheet';
+import { WriteInputs } from '..';
 
 interface LetterWriteBottomProps {
   isPending: boolean;
@@ -11,21 +14,48 @@ interface LetterWriteBottomProps {
 
 const LetterWriteBottom = ({ isPending }: LetterWriteBottomProps) => {
   const navigate = useNavigate();
+  const { value, on, off } = useBoolean(false);
+
+  const { watch } = useFormContext<WriteInputs>();
+
+  const handleBackward = () => {
+    if (watch('age').length !== 0 || watch('content')) {
+      on();
+    } else {
+      navigate(-1);
+    }
+  };
 
   return (
-    <Navbar css={style.navbar}>
-      <Button
-        onClick={() => navigate(ROUTER_PATHS.ROOT)}
-        type="button"
-        variant="secondary"
-        size="sm"
-      >
-        취소
-      </Button>
-      <Button disabled={isPending} type="submit" variant="primary" size="sm">
-        {isPending ? <LoadingSpinner /> : '보내기'}
-      </Button>
-    </Navbar>
+    <>
+      <Navbar css={style.navbar}>
+        <Button
+          onClick={handleBackward}
+          type="button"
+          variant="secondary"
+          size="sm"
+        >
+          취소
+        </Button>
+        <Button disabled={isPending} type="submit" variant="primary" size="sm">
+          {isPending ? <LoadingSpinner /> : '보내기'}
+        </Button>
+      </Navbar>
+      <BottomSheet open={value} onOpen={on} onClose={off}>
+        <BottomSheet.Title>편지쓰기를 취소할까요?</BottomSheet.Title>
+        <BottomSheet.Description>
+          편지 작성 취소시, <br /> 작성중인 글과 사진은 저장되지 않아요.
+        </BottomSheet.Description>
+        <BottomSheet.ButtonSection>
+          <Button variant="cancel" onClick={off}>
+            계속 쓰기
+          </Button>
+          <Button variant="danger" onClick={() => navigate(-1)}>
+            작성 취소
+          </Button>
+        </BottomSheet.ButtonSection>
+      </BottomSheet>
+    </>
   );
 };
 
