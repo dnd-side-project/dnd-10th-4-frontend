596fec0f5eea2c439e54f513fbe7da1de9282718
bbearcookie <aodem@naver.com>
2024-02-23 19:27:28 +0900

    💄 design: 카카오 리다이렉션 페이지에 LoadingScreen 적용

--- src/pages/SigninKakaoPage/index.tsx
+++ src/pages/SigninKakaoPage/index.tsx
@@ -5,6 +5,7 @@ import { ROUTER_PATHS } from '@/router';
 import authAPI from '@/api/auth/apis';
 import STORAGE_KEYS from '@/constants/storageKeys';
 import memberOptions from '@/api/member/queryOptions';
+import LoadingScreen from '@/components/LoadingScreen';
 
 const SigninKakaoPage = () => {
   const [searchParams] = useSearchParams();
@@ -34,7 +35,7 @@ const SigninKakaoPage = () => {
     })();
   }, []);
 
-  return <div>Loading...</div>;
+  return <LoadingScreen />;
 };
 
 export default SigninKakaoPage;
