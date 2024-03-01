abb6d20fc4a3c39945911a18df8df63ec35182f7
bbearcookie <aodem@naver.com>
2024-03-01 22:56:39 +0900

    📝 style: msw 핸들러에 주석 추가

--- src/mocks/handlers/auth.ts
+++ src/mocks/handlers/auth.ts
@@ -4,11 +4,13 @@ import { API_PATHS } from '@/constants/routerPaths';
 import { authResolvers } from '../resolvers/auth';
 
 const authHandler = [
+  /** 카카오 인가 코드 전송 후 로그인 토큰 받아오기 */
   http.post(
     baseURL(API_PATHS.AUTH_LOGIN_KAKAO),
     authResolvers.postLoginKakao.firstLogin,
   ),
 
+  /** 토큰 재발급 */
   http.get(baseURL(API_PATHS.AUTH_REISSUE), authResolvers.getReissue.success),
 
   /** 액세스 토큰이 필요한 API를 시뮬레이션하기 위해 테스트 코드에서만 사용되는 API */
--- src/mocks/handlers/letter.ts
+++ src/mocks/handlers/letter.ts
@@ -5,66 +5,79 @@ import withAuth from '../middlewares/withAuth';
 import { letterResolvers } from '../resolvers/letter';
 
 const letterHandler = [
+  /** 받은 편지 전체 조회 */
   http.get(
     baseURL(API_PATHS.LETTER_RECEPTION),
     withAuth(letterResolvers.getLetterReception.success),
   ),
 
+  /** 보낸 편지 단건 조회 */
   http.get(
     baseURL(API_PATHS.LETTER_RECEPTION_DETAIL(':letterId')),
     letterResolvers.getLetterReceptionDetail.success,
   ),
 
+  /** 답장 받은 편지 페이징 조회 */
   http.get(
     baseURL(API_PATHS.LETTER_REPLY),
     withAuth(letterResolvers.getLetterReply.success),
   ),
 
+  /** 답장 받은 편지 단건 조회 */
   http.get(
     baseURL(API_PATHS.LETTER_REPLY_DETAIL(':letterId')),
     withAuth(letterResolvers.getLetterReplyDetail.success),
   ),
 
+  /** 보관한 편지 페이징 조회 */
   http.get(
     baseURL(API_PATHS.LETTER_STORAGE),
     withAuth(letterResolvers.getLetterStorage.success),
   ),
 
+  /** 내가 보낸 편지 페이징 조회 */
   http.get(
     baseURL(API_PATHS.LETTER_SEND),
     withAuth(letterResolvers.getLetterSend.success),
   ),
 
+  /** 편지 작성 */
   http.post(
     baseURL(API_PATHS.LETTER),
     withAuth(letterResolvers.postLetter.success),
   ),
 
+  /** 받은 편지 답장 */
   http.patch(
     baseURL(API_PATHS.LETTER_RECEPTION_REPLY_DETAIL(':letterId')),
     withAuth(letterResolvers.patchLetterReceptionReplyDetail.success),
   ),
 
+  /** 받은 편지 패스 */
   http.patch(
     baseURL(API_PATHS.LETTER_RECEPTION_PASS_DETAIL(':letterId')),
     withAuth(letterResolvers.patchLetterReceptionPassDetail.success),
   ),
 
+  /** 답장 받은 편지 보관함에 보관 */
   http.patch(
     baseURL(API_PATHS.LETTER_REPLY_STORAGE_DETAIL(':letterId')),
     withAuth(letterResolvers.patchLetterReplyStorageDetail.success),
   ),
 
+  /** 온보딩 편지 보관함에 보관 */
   http.patch(
     baseURL(API_PATHS.LETTER_ONBOARDING_STORAGE_DETAIL(':letterId')),
     withAuth(letterResolvers.patchLetterOnboardingStorageDetail.success),
   ),
 
+  /** 보관된 편지 보관 취소 */
   http.patch(
     baseURL(API_PATHS.LETTER_STORAGE_DETAIL(':letterId')),
     withAuth(letterResolvers.patchLetterStorageDetail.success),
   ),
 
+  /** 보낸 편지 삭제 */
   http.patch(
     baseURL(API_PATHS.LETTER_SEND_DETAIL(':letterId')),
     withAuth(letterResolvers.patchLetterSendDetail.success),
