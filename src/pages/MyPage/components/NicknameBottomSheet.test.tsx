6d017ecd4bbf7a38c58bc5b33843eb4ae3801c3d
bbearcookie <aodem@naver.com>
2024-03-04 01:54:12 +0900

    ✅ test: NicknameBottomSheet 테스트 코드에 인터랙션 묶음

--- src/pages/MyPage/components/NicknameBottomSheet.test.tsx
+++ src/pages/MyPage/components/NicknameBottomSheet.test.tsx
@@ -9,18 +9,30 @@ import { API_PATHS } from '@/constants/routerPaths';
 import { memberResolvers } from '@/mocks/resolvers/member';
 import NicknameBottomSheet from './NicknameBottomSheet';
 
-it('닉네임 변경 아이콘을 클릭하면 getRandomItem 함수가 실행된다', async () => {
-  const getRandomItem = vi.spyOn(arrayUtils, 'getRandomItem');
-  const { result: modalProps } = renderHook(() => useBoolean(true));
-  const { user } = render(<NicknameBottomSheet {...modalProps.current} />);
+describe('인터랙션 테스트', () => {
+  it('닉네임 변경 아이콘을 클릭하면 getRandomItem 함수가 실행된다', async () => {
+    const getRandomItem = vi.spyOn(arrayUtils, 'getRandomItem');
+    const { result: modalProps } = renderHook(() => useBoolean(true));
+    const { user } = render(<NicknameBottomSheet {...modalProps.current} />);
 
-  const changeNickname = await screen.findByTestId('change-nickname');
-  await user.click(changeNickname);
+    const changeNickname = await screen.findByTestId('change-nickname');
+    await user.click(changeNickname);
 
-  expect(getRandomItem).toHaveBeenCalledTimes(1);
+    expect(getRandomItem).toHaveBeenCalledTimes(1);
+  });
+
+  it('닫기 버튼을 클릭하면 모달이 닫힌다', async () => {
+    const { result: modalProps } = renderHook(() => useBoolean(true));
+    const { user } = render(<NicknameBottomSheet {...modalProps.current} />);
+
+    const closeButton = await screen.findByText('닫기');
+    await user.click(closeButton);
+
+    expect(modalProps.current.value).toBe(false);
+  });
 });
 
-describe('닉네임 변경 API 호출', () => {
+describe('API 테스트', () => {
   it('[성공] 변경 완료 버튼을 클릭하면 API 호출이 발생하고 처리 중에는 버튼이 비활성화된다. 요청이 성공하면 성공 토스트가 나타난다.', async () => {
     const { result: modalProps } = renderHook(() => useBoolean(true));
     const { user } = render(<NicknameBottomSheet {...modalProps.current} />);
@@ -32,6 +44,7 @@ describe('닉네임 변경 API 호출', () => {
 
     await waitFor(() => {
       expect(submitButton).not.toBeDisabled();
+      expect(modalProps.current.value).toBe(false);
       expect(toast.success).toHaveBeenCalledTimes(1);
     });
   });
@@ -52,17 +65,8 @@ describe('닉네임 변경 API 호출', () => {
 
     await waitFor(() => {
       expect(submitButton).not.toBeDisabled();
+      expect(modalProps.current.value).toBe(true);
       expect(toast.error).toHaveBeenCalledTimes(1);
     });
   });
-});
-
-it('닫기 버튼을 클릭하면 모달이 닫힌다', async () => {
-  const { result: modalProps } = renderHook(() => useBoolean(true));
-  const { user } = render(<NicknameBottomSheet {...modalProps.current} />);
-
-  const closeButton = await screen.findByText('닫기');
-  await user.click(closeButton);
-
-  expect(modalProps.current.value).toBe(false);
 });
