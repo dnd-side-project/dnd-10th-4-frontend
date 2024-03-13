f4a6fb26ab795fdec90a044a0ac09c5696cdfac8
bbearcookie <aodem@naver.com>
2024-03-14 00:05:40 +0900

    ➖ remove: readLetter storage key 상수 제거

--- src/constants/storageKeys.ts
+++ src/constants/storageKeys.ts
@@ -1,7 +1,6 @@
 const STORAGE_KEYS = {
   accessToken: 'accessToken',
   refreshToken: 'refreshToken',
-  readLetter: 'readLetter',
   letterSlide: 'letterSlide',
   isPlayingMusic: 'isPlayingMusic',
 } as const;
