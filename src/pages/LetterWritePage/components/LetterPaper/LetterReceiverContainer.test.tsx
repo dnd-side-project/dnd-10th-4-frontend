f80815f5dad56eb05cb456b4ce234d1d2fc5cd50
easyhyun00 <leejihyun0324@gmail.com>
2024-03-15 15:31:36 +0900

    ✅ test: 편지 받는 사람 수정 완료 후 렌더링 테스트

--- src/pages/LetterWritePage/components/LetterPaper/LetterReceiverContainer.test.tsx
+++ src/pages/LetterWritePage/components/LetterPaper/LetterReceiverContainer.test.tsx
@@ -30,6 +30,23 @@ const LetterReceiverContainerComponent = () => {
   );
 };
 
+const LetterReceiverSelectedComponent = () => {
+  const methods = useForm<WriteInputs>({
+    resolver: zodResolver(writeSchema),
+    defaultValues: {
+      age: [10, 40],
+      content: '',
+      gender: '나와 같은 성별에게 보내기',
+      worryType: 'LOVE',
+    },
+  });
+  return (
+    <FormProvider {...methods}>
+      <LetterReceiverContainer />,
+    </FormProvider>
+  );
+};
+
 describe('인터랙션 테스트', () => {
   it('누구에게 보낼까요? 를 누르면 바텀시트가 열린다.', async () => {
     const { user } = render(<LetterReceiverContainerComponent />);
@@ -129,5 +146,52 @@ describe('편지 받는 사람 선택 닫기 후, 렌더링 테스트', () => {
       selector: 'span',
     });
     expect(receiverContainer).toBeVisible();
+  });
+});
+
+describe('편지 받는 사람 수정 완료 후, 렌더링 테스트', () => {
+  let user: UserEvent;
+
+  beforeEach(async () => {
+    const { user: userEvent } = render(<LetterReceiverSelectedComponent />);
+    user = userEvent;
+
+    const ageTag = await screen.findByText('동성에게');
+
+    await user.click(ageTag);
+  });
+
+  it('나이, 성별, 고민이 정해져 있는 상태에서, 성별을 모두에게 보내기 재선택 후 완료 버튼을 누르면 모두에게가 보인다.', async () => {
+    const genderchip = screen.getByRole('button', {
+      name: '모두에게 보내기',
+    });
+    expect(genderchip).toHaveAttribute('aria-selected', 'false');
+
+    await user.click(genderchip);
+
+    expect(genderchip).toHaveAttribute('aria-selected', 'true');
+
+    const buttonElement = screen.getByRole('button', { name: '완료' });
+    await user.click(buttonElement);
+
+    const genderTag = await screen.findByText('모두에게');
+    expect(genderTag).toBeVisible();
+  });
+
+  it('나이, 성별, 고민이 정해져 있는 상태에서, 고민을 학업으로 재선택 후 완료 버튼을 누르면 학업이 보인다.', async () => {
+    const worrychip = screen.getByRole('button', {
+      name: '학업',
+    });
+    expect(worrychip).toHaveAttribute('aria-selected', 'false');
+    await user.click(worrychip);
+
+    const buttonElement = screen.getByRole('button', { name: '완료' });
+    await user.click(buttonElement);
+
+    const worry = await screen.getAllByText('학업', {
+      selector: 'button',
+    });
+    const hashSymbol = within(worry[0]).getByText('#');
+    expect(hashSymbol).toBeVisible();
   });
 });
