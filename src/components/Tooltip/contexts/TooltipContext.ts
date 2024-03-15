69d7a18285296807931e583bcb0d1ceba694b9a5
bbearcookie <aodem@naver.com>
2024-03-15 20:37:53 +0900

    🔨 refactor: TooltipContext의 기본 값 null로 수정

--- src/components/Tooltip/contexts/TooltipContext.ts
+++ src/components/Tooltip/contexts/TooltipContext.ts
@@ -3,10 +3,16 @@ import { createContext, useContext } from 'react';
 const TooltipContext = createContext<{
   isOpen: boolean;
   setIsOpen: (open: boolean) => void;
-}>({
-  isOpen: false,
-  setIsOpen: () => {},
-});
+} | null>(null);
 
 export const TooltipProvider = TooltipContext.Provider;
-export const useTooltipContext = () => useContext(TooltipContext);
+
+export const useTooltipContext = () => {
+  const context = useContext(TooltipContext);
+
+  if (!context) {
+    throw new Error('useTooltipContext must be used within a TooltipProvider');
+  }
+
+  return context;
+};
