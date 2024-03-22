c209a84f412b1c7b0fba1122207a05e43f9177d4
easyhyun00 <leejihyun0324@gmail.com>
2024-03-22 13:53:01 +0900

    ✨ feat: 신고 버튼 클릭 시  신고하기 기능 구현

--- src/components/ReportBottomSheet/index.tsx
+++ src/components/ReportBottomSheet/index.tsx
@@ -1,38 +1,81 @@
 import { useForm, useWatch } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import { useNavigate, useParams } from 'react-router-dom';
+import z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import { isAxiosError } from 'axios';
 import useBoolean from '@/hooks/useBoolean';
+import { REPORT_TYPE_DICT, type ReportType } from '@/constants/report';
+import reportAPI from '@/api/report/api';
+import ERROR_RESPONSES from '@/constants/errorMessages';
+import { ROUTER_PATHS } from '@/constants/routerPaths';
 import BottomSheet from '../BottomSheet';
 import Button from '../Button';
+import LoadingSpinner from '../LoadingSpinner';
 import style from './styles';
 import Radio from './components/Radio';
 
 interface ReportBottomSheetProps extends ReturnType<typeof useBoolean> {}
 
-const reportList = [
-  { text: '욕설', value: 'a' },
-  { text: '광고 및 도배', value: 'b' },
-  { text: '음란성 게시물', value: 'c' },
-  { text: '그 외 부적절한 내용', value: 'd' },
-];
+const reportList = Object.entries(REPORT_TYPE_DICT).map(([key, value]) => ({
+  text: value,
+  value: key,
+}));
 
-type ReportInputs = {
-  reportType: null | string;
-  reportContent: string;
-};
+const [report, ...otherReport] = Object.keys(REPORT_TYPE_DICT) as ReportType[];
+
+const reportSchema = z.object({
+  reportType: z.enum([report, ...otherReport]),
+  reportContent: z.string(),
+});
+
+export type ReportInputs = z.infer<typeof reportSchema>;
 
 const ReportBottomSheet = ({ value, on, off }: ReportBottomSheetProps) => {
+  const navigate = useNavigate();
+  const { letterId } = useParams();
+
   const methods = useForm<ReportInputs>({
+    resolver: zodResolver(reportSchema),
     defaultValues: {
-      reportType: null,
       reportContent: '',
     },
   });
 
-  const { register, handleSubmit, control } = methods;
+  const { register, handleSubmit, control, reset } = methods;
   const selectedValue = useWatch({ control, name: 'reportType' });
 
-  const onSubmit = (data: ReportInputs) => {
-    console.log(data);
-    off();
+  const { mutate, isPending } = useMutation({
+    mutationFn: reportAPI.postReportSend,
+  });
+
+  const onSubmit = async (data: ReportInputs) => {
+    mutate(
+      { letterId: Number(letterId), ...data },
+      {
+        onSuccess: () => {
+          toast.success('신고가 접수되었어요', {
+            position: 'bottom-center',
+            autoClose: 1500,
+          });
+        },
+        onError: (err) => {
+          if (isAxiosError(err)) {
+            if (
+              err.response?.data === ERROR_RESPONSES.memberNotFound ||
+              err.response?.data === ERROR_RESPONSES.accessDeniedLetter
+            ) {
+              navigate(ROUTER_PATHS.ROOT);
+            }
+          }
+        },
+        onSettled: () => {
+          off();
+          reset();
+        },
+      },
+    );
   };
 
   return (
@@ -70,7 +113,7 @@ const ReportBottomSheet = ({ value, on, off }: ReportBottomSheetProps) => {
             취소하기
           </Button>
           <Button type="submit" variant="danger">
-            신고하기
+            {isPending ? <LoadingSpinner /> : <>신고하기</>}
           </Button>
         </BottomSheet.ButtonSection>
       </form>
