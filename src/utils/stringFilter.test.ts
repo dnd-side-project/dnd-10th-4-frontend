da138886a7a4338d69d7bfabe766d50c7e2492b6
easyhyun00 <leejihyun0324@gmail.com>
2024-03-23 16:25:06 +0900

    ✅ test: 필터링 결과 일치여부 테스트코드 작성

--- src/utils/stringFilter.test.ts
+++ src/utils/stringFilter.test.ts
@@ -7,25 +7,31 @@ const text100 =
 const text300 =
   '300 삼백 글자 텍스트 이 문장에는 가끔 욕이 들어갑니다!!! 금칙어 필터링 테스트 시발 300 삼백 글자 텍스트 이 문장에는 가끔 욕이 들어갑니다!!! 금칙어 필터링 테스트 시발 300 삼백 글자 텍스트 이 문장에는 가끔 욕이 들어갑니다!!! 금칙어 필터링 테스트 시발 300 삼백 글자 텍스트 이 문장에는 가끔 욕이 들어갑니다!!! 금칙어 필터링 테스트 시발 300 삼백 글자 텍스트 이 문장에는 가끔 욕이 들어갑니다!!! 금칙어 필터링 테스트 시발 300 삼백 글자 텍스트 이 문장에는 가끔 욕이 들어갑니다!!!!! 금칙어 필터링';
 
+const text10Filter = '안녕하세요 ***야';
+const text100Filter =
+  '** ** ** ** ** ***** 개** *** 욕 하는 중 ** ** ** ** ** ***** 개** *** 욕 하는 중 ** ** ** ** ** ***** 개** ***';
+const text300Filter =
+  '300 삼백 글자 텍스트 이 문장에는 가끔 욕이 들어갑니다!!! 금칙어 필터링 테스트 ** 300 삼백 글자 텍스트 이 문장에는 가끔 욕이 들어갑니다!!! 금칙어 필터링 테스트 ** 300 삼백 글자 텍스트 이 문장에는 가끔 욕이 들어갑니다!!! 금칙어 필터링 테스트 ** 300 삼백 글자 텍스트 이 문장에는 가끔 욕이 들어갑니다!!! 금칙어 필터링 테스트 ** 300 삼백 글자 텍스트 이 문장에는 가끔 욕이 들어갑니다!!! 금칙어 필터링 테스트 ** 300 삼백 글자 텍스트 이 문장에는 가끔 욕이 들어갑니다!!!!! 금칙어 필터링';
+
 describe('욕설 필터링 - 정규식 사용', () => {
   it('정규식 사용', () => {
     console.time();
-    console.log(stringFilter1(text));
+    expect(stringFilter1(text)).toBe(text);
     console.timeEnd();
   });
   it('정규식 사용 - 10글자', () => {
     console.time();
-    console.log(stringFilter1(text10));
+    expect(stringFilter1(text10)).toBe(text10Filter);
     console.timeEnd();
   });
   it('정규식 사용 - 100글자', () => {
     console.time();
-    console.log(stringFilter1(text100));
+    expect(stringFilter1(text100)).toBe(text100Filter);
     console.timeEnd();
   });
   it('정규식 사용 - 300글자', () => {
     console.time();
-    console.log(stringFilter1(text300));
+    expect(stringFilter1(text300)).toBe(text300Filter);
     console.timeEnd();
   });
 });
@@ -33,22 +39,22 @@ describe('욕설 필터링 - 정규식 사용', () => {
 describe('욕설 필터링 - 반복문 사용', () => {
   it('반복문 사용', () => {
     console.time();
-    console.log(stringFilter2(text));
+    expect(stringFilter1(text)).toBe(text);
     console.timeEnd();
   });
   it('반복문 사용 - 10글자', () => {
     console.time();
-    console.log(stringFilter2(text10));
+    expect(stringFilter2(text10)).toBe(text10Filter);
     console.timeEnd();
   });
   it('반복문 사용 - 100글자', () => {
     console.time();
-    console.log(stringFilter2(text100));
+    expect(stringFilter2(text100)).toBe(text100Filter);
     console.timeEnd();
   });
   it('반복문 사용 - 300글자', () => {
     console.time();
-    console.log(stringFilter2(text300));
+    expect(stringFilter2(text300)).toBe(text300Filter);
     console.timeEnd();
   });
 });
