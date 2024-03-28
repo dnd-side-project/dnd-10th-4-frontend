5b4544364f519b3fd542703f89bc8afe35ad47cd
bbearcookie <aodem@naver.com>
2024-03-28 09:06:48 +0900

    🔨 refactor: Tabs 합성 컴포넌트로 리팩토링

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,45 +1,30 @@
 import React from 'react';
 import * as T from '@radix-ui/react-tabs';
-import styles, { type TabsVariant } from './styles';
+import { type TabsVariant } from './styles';
+import { TabsProvider } from './contexts/TabsContext';
+import List from './components/List';
+import Trigger from './components/Trigger';
+import Content from './components/Content';
 
 interface TabsProps {
   /** 탭의 배경 색상과 스타일 등 테마를 지정합니다. */
   variant?: TabsVariant;
   /** 기본으로 활성화 되어 있을 탭 아이템의 key를 지정합니다.  */
   defaultValue?: string;
-  /** 탭 아이템의 목록을 지정합니다. */
-  tabItems: {
-    key: string;
-    label: React.ReactNode;
-    content: React.ReactNode;
-  }[];
+  /** Tabs 컴포넌트 안에 포함될 내용입니다. */
+  children?: React.ReactNode;
 }
 
-const Tabs = ({ variant = 'primary', defaultValue, tabItems }: TabsProps) => {
-  defaultValue = defaultValue ?? tabItems[0].key;
-
+const Tabs = ({ variant = 'primary', defaultValue, children }: TabsProps) => {
   return (
-    <T.Root defaultValue={defaultValue}>
-      <T.List css={styles.list(variant)} aria-label="Tabs">
-        {tabItems.map((tabItem) => (
-          <T.Trigger
-            css={styles.trigger(variant)}
-            key={tabItem.key}
-            value={tabItem.key}
-          >
-            {tabItem.label}
-          </T.Trigger>
-        ))}
-      </T.List>
-
-      {tabItems.map((tabItem) => (
-        <T.Content key={tabItem.key} value={tabItem.key}>
-          {tabItem.content}
-        </T.Content>
-      ))}
-    </T.Root>
+    <TabsProvider value={{ variant }}>
+      <T.Root defaultValue={defaultValue}>{children}</T.Root>
+    </TabsProvider>
   );
 };
 
+Tabs.List = List;
+Tabs.Trigger = Trigger;
+Tabs.Content = Content;
+
 export default Tabs;
-export { default as tabsStyles } from './styles';
