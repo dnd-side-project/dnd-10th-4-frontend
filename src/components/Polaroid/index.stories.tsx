07ea09d82e73efb4bd252386c95f89ac2136820d
easyhyun00 <leejihyun0324@gmail.com>
2024-04-06 22:36:24 +0900

    🔨 refactor: Polaroid 컴포넌트 스토리북  수정

--- src/components/Polaroid/index.stories.tsx
+++ src/components/Polaroid/index.stories.tsx
@@ -1,9 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { css } from '@emotion/react';
-import useBoolean from '@/hooks/useBoolean';
 import LetterCard from '../LetterCard';
-import Modal from '../Modal';
-import Button from '../Button';
 import Polaroid from '.';
 
 const meta = {
@@ -23,64 +19,22 @@ export const Primary: Story = {
   },
 };
 
-const style = {
-  modalContainer: css`
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    justify-content: center;
-    align-items: center;
-    margin-inline: 1rem;
-    margin-top: 3rem;
-
-    button {
-      padding-inline: 40px;
-    }
-  `,
-};
-
 export const 편지지_폴라로이드: StoryObj = {
-  render: () => {
-    const PolaroidComponent = () => {
-      const { value: isOpen, on: open, off: close } = useBoolean(false);
-
-      return (
-        <>
-          <LetterCard isOpen={true}>
-            <Polaroid
-              onClick={open}
-              imgUrl="https://cdn.pixabay.com/photo/2016/11/23/13/48/beach-1852945_1280.jpg"
-            />
-          </LetterCard>
-          <Modal close={close} isOpen={isOpen}>
-            <div css={style.modalContainer}>
-              <Polaroid
-                imgUrl="https://cdn.pixabay.com/photo/2016/11/23/13/48/beach-1852945_1280.jpg"
-                size="lg"
-              />
-              <Button variant="secondary" size="sm" onClick={close}>
-                닫기
-              </Button>
-            </div>
-          </Modal>
-        </>
-      );
-    };
-    return <PolaroidComponent />;
-  },
+  render: () => (
+    <>
+      <LetterCard isOpen={true}>
+        <Polaroid imgUrl="https://cdn.pixabay.com/photo/2016/11/23/13/48/beach-1852945_1280.jpg" />
+      </LetterCard>
+    </>
+  ),
 };
 
 export const 사진_추가_폴라로이드: StoryObj = {
-  render: () => {
-    const PolaroidImage = () => {
-      return (
-        <>
-          <LetterCard isOpen={true}>
-            <Polaroid />
-          </LetterCard>
-        </>
-      );
-    };
-    return <PolaroidImage />;
-  },
+  render: () => (
+    <>
+      <LetterCard isOpen={true}>
+        <Polaroid />
+      </LetterCard>
+    </>
+  ),
 };
